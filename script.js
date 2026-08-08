74daf76903c4d84242b3339d78edaa56f6c665d0
AJAY DHAKREY <107066854+AjayDhakrey@users.noreply.github.com>
2026-08-08 12:53:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 //your JS code here. If required.
-
 let year = 2002;
-function checkyear(){
-if(year % 400 ===0||year % 4 ===0 && year % 100!==0)
-	return 366}
-return 365;
+
+function checkyear() {
+    if (year % 400 === 0 || (year % 4 === 0 && year % 100 !== 0)) {
+        return 366;
+    }
+
+    return 365;
+}
+
+console.log(checkyear());
 	
